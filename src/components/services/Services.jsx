f0809a5ecc5ccf15423e3a272e55b84a87b6ccb3
rohitdas13595 <rohitdas10595@gmail.com>
2022-03-14 16:36:49 +0530

    Added Services Template

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,105 @@
 import React from 'react'
 import './Services.css'
 
+import { BsCheckLg } from 'react-icons/bs';
+
 function Services() {
   return (
-    <section id="services">Services</section>
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BsCheckLg className='service__list__icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheckLg className='service__list__icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheckLg className='service__list__icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheckLg className='service__list__icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheckLg className='service__list__icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>Front End Developer</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BsCheckLg className='service__list__icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheckLg className='service__list__icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheckLg className='service__list__icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheckLg className='service__list__icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheckLg className='service__list__icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>FullStack Developer</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BsCheckLg className='service__list__icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheckLg className='service__list__icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheckLg className='service__list__icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheckLg className='service__list__icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheckLg className='service__list__icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            
+          </ul>
+        </article>
+      </div>
+
+    </section>
   )
 }
 
